082ce02e7cefb9c573b5a070028d66515f13ca2f
FaithMo <faithmofokeng5@gmail.com>
2019-10-28 17:16:33 +0200

    modified negative numbers spec

--- spec/string_calculatorSpec.js
+++ spec/string_calculatorSpec.js
@@ -1,44 +1,55 @@
+var searchNeg = /[-]/g;
 
-describe("Calculate a string", function(){
+showError = num => {
+  var testNeg = searchNeg.test(num); //boolean
+  if (!testNeg) {
+    return "Great!";
+  } else {
+    return "negatives not allowed";
+  }
+};
 
-	it("should return 0 for a empty string", function(){
-		let result1 = add("");
-		expect(result1).toBe(0);
-	});
+describe("String calculator", () => {
+  it("should throw an error for negative numbers", () => {
+	var result6 = showError("-35");
+    var jasmineError = () => {
+      throw result6;
+    };
+    expect(jasmineError).toThrow("negatives not allowed");
+  });
 
-	it("should handle one number", function(){
-		let result3 = add("1");
-		expect(result3).toBe(1);
-	});
+  it("should return 0 for a empty string", function() {
+    let result1 = add("");
+    expect(result1).toBe(0);
+  });
 
-	it("should handle two numbers", function(){
-		let result2 = add("1,2");
-		expect(result2).toBe(3);
-	});
+  it("should handle one number", function() {
+    let result3 = add("1");
+    expect(result3).toBe(1);
+  });
 
-	it("should handle an unknown amount of numbers", function(){
-		let result4 = add("1,2,3,4");
-		expect(result4).toBe(10);
-	});
+  it("should handle two numbers", function() {
+    let result2 = add("1,2");
+    expect(result2).toBe(3);
+  });
 
-	it("should split numbers with any detected delimiters", function(){
-		let result5 = add("//;\n1;2");
-		expect(result5).toBe(3);
-	});
+  it("should handle an unknown amount of numbers", function() {
+    let result4 = add("1,2,3,4");
+    expect(result4).toBe(10);
+  });
 
-	it("should not handle negative numbers", function(){
-		let result6 = add("-3,5");
-		expect(result6).toBe("negatives not allowed");
-	});
+  it("should split numbers with any detected delimiters", function() {
+    let result5 = add("//;\n1;2");
+    expect(result5).toBe(3);
+  });
 
-	it("should handle delimiters of any length", function(){
-		let result7 = add("//[***]\n1***2***3");
-		expect(result7).toBe(6);
-	});
-	
-	it("should ignore numbers greater than 1000", function(){
-		let result8 = add("1003,3");
-		expect(result8).toBe(3);
-	});
+  it("should handle delimiters of any length", function() {
+    let result7 = add("//[***]\n1***2***3");
+    expect(result7).toBe(6);
+  });
 
-})
+  it("should ignore numbers greater than 1000", function() {
+    let result8 = add("1003,3");
+    expect(result8).toBe(3);
+  });
+});
--- src/string_calcu.js
+++ src/string_calcu.js
@@ -1,40 +1,30 @@
 var num;
-function add(num){
+function add(num) {
+  if (num !== "") {
+    var search = /[0-9]+/g;
+    var searchNeg = /-?\d+/g;
+    var testSearch = searchNeg.test(num);
+    var searchNum2 = num.match(search).map(Number);
+    var searchNum3 = num.match(searchNeg).map(Number);
 
-    if(num !== ""){
+    if (testSearch == true) {
+      var count2 = 0;
 
-        var search = /[0-9]+/g;
-        var searchNeg = /-?\d+/g;
-        var testSearch = searchNeg.test(num);
-        var searchNum2 = num.match(search).map(Number);
-        var searchNum3 = num.match(searchNeg).map(Number);
-
-        if(testSearch == true){
-
-            var count2 = 0;
-
-            for(var j in searchNum2){
-
-                if(searchNum3[j]<0){
-
-                    return "negatives not allowed";
-
-                }else if(searchNum2[j]>1000){
-
-                    searchNum2[j] == 0;
-                    count2 += 0;
-
-                }else{
-                    count2 += searchNum3[j];
-                }
-            }
-            return count2;
+      for (var j in searchNum2) {
+        if (searchNum3[j] < 0) {
+          throw new Error ("negatives not allowed");
+        } else if (searchNum2[j] > 1000) {
+          searchNum2[j] == 0;
+          count2 += 0;
+        } else {
+          count2 += searchNum3[j];
         }
-        
-    }else{
-        return 0;
+      }
+      return count2;
     }
+  } else {
+    return 0;
+  }
 }
-
 
 add("2000,6");
